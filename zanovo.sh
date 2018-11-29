491f7e8280d18bc8f9ab7bb3fc3d63626bd28b01
Noel <anufreos@gmail.com>
2018-11-29 15:11:34 +0000

    add jenkins_shell

--- zanovo.sh
+++
@@ -1,6 +0,0 @@
-docker stop $(docker ps -aq) 
-docker rm jenkins 
-docker rm jenkins-data
-docker build -t identijenk .
-docker run --name jenkins-data identijenk echo "qweqe" 
-docker run -d --name jenkins -p 8080:8080 --volumes-from jenkins-data -v /var/run/docker.sock:/var/run/docker.sock identijenk
